fc22a059b9b279df7e88b37223fd8772f523973c
xpolny <94650193+xpolny@users.noreply.github.com>
2022-01-27 22:31:20 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -64,9 +64,9 @@ with open(csvpath, newline = '') as csvfile:
     
     PyBank = open("output.txt","w+")
     PyBank.write("Financial Analysis") 
-    PyBank.write('\n' +"Total Months" + str(total_months)) 
-    PyBank.write('\n' +"Total Amount" + str(total_revenue)) 
-    PyBank.write('\n' +"Average" + str(average_change)) 
+    PyBank.write('\n' +"Total Months: " + str(total_months)) 
+    PyBank.write('\n' +"Total Amount: " + str(total_revenue)) 
+    PyBank.write('\n' +"Average Change: " + str(average_change)) 
     PyBank.write('\n' +greatest_inc_month) 
     PyBank.write('\n' +str(high))
     PyBank.write('\n' +greatest_dec_month) 
