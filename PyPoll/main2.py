c95ca69bc3889f21c4b7613cd655ef4e4e8b9351
xpolny <94650193+xpolny@users.noreply.github.com>
2022-01-27 19:16:12 -0800

    Add files via upload

---
+++ PyPoll/main2.py
@@ -0,0 +1,48 @@
+import os 
+import csv
+
+#CSV File
+csvpath = os.path.join('election_data.csv')
+
+#Variables
+poll_data={}
+total_votes = 0
+with open(csvpath, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+    next(csvread, None)
+
+    for row in csvread:
+        total_votes += 1
+        if row[2] in poll_data.keys():
+            poll_data[row[2]] = poll_data[row[2]] + 1
+        else:
+            poll_data[row[2]] = 1 
+candidates = []  
+tot_num_votes = []
+
+# Total votes
+for key, value in poll_data.items():
+    candidates.append(key)
+    tot_num_votes.append(value)
+  
+# Percentage of votes
+votes_percent =[]
+for n in tot_num_votes:
+    votes_percent.append(round(n/total_votes * 100, 1))
+ 
+# Winner
+clean_data = list(zip(candidates, tot_num_votes, votes_percent))
+
+winner_list = []
+for name in clean_data:
+    if max(tot_num_votes) == name[1]:
+        winner_list.append(name[0])
+winner = winner_list[0]
+
+# Print
+print ("Election results :")
+print(total_votes) 
+print(tot_num_votes)  
+print(candidates)  
+print(votes_percent) 
+print(winner)
